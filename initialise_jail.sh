621e355ad0af2a817905472e4624e569c25c16a5
Eric LEVEAU <ptit6sky@gmail.com>
2013-03-04 14:07:36 +0100

    Fix path

--- initialise_jail.sh
+++ initialise_jail.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
   #Extract jail-min.tar.gz
-  if [ ! -d ../jail ] then
+  if [ ! -d /home/admin/jail ] then
   echo "Creating jail directory"
   fi
 
-  if [ -f ../jail-min.tar.gz] else
-      tar -xzf ../jail-min.tar.gz -C ../jail
+  if [ -f ./jail-min.tar.gz] else
+      tar -xzf ./jail-min.tar.gz -C /home/admin/jail
       #Copy needed etc files
-      cp -rf /etc/hosts ../jail/etc
-      cp -rf /etc/nsswitch.conf ../jail/etc
-      cp -rf /etc/login.defs ../jail/etc
-      cp -rf /etc/pam.d ../jail/etc
-      cp -rf /etc/passwd ../jail/etc
-      cp -rf /etc/resolv.conf ../jail/etc
-      cp -rf /etc/security ../jail/etc
-      cp -rf /etc/shadow ../jail/etc
-      cp -rf /usr/local/bin ../jail/usr/local
-      cp -rf /usr/local/lib ../jail/usr/local
+      cp -rf /etc/hosts /home/admin/jail/etc
+      cp -rf /etc/nsswitch.conf /home/admin/jail/etc
+      cp -rf /etc/login.defs /home/admin/jail/etc
+      cp -rf /etc/pam.d /home/admin/jail/etc
+      cp -rf /etc/passwd /home/admin/jail/etc
+      cp -rf /etc/resolv.conf /home/admin/jail/etc
+      cp -rf /etc/security /home/admin/jail/etc
+      cp -rf /etc/shadow /home/admin/jail/etc
+      cp -rf /usr/local/bin /home/admin/jail/usr/local
+      cp -rf /usr/local/lib /home/admin/jail/usr/local
   then
-  echo "Can't find ../jail-min.tar.gz"
+  echo "Can't find ./jail-min.tar.gz"
   fi
