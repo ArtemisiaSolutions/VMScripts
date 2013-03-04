4e58d39c2ba97e5145dc5b0c89f2c592d95bbbf9
Eric LEVEAU <ptit6sky@gmail.com>
2013-03-04 13:27:12 +0100

    Add new user notification Fix

--- add_bin_jail.sh
+++ add_bin_jail.sh
@@ -1,121 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $# -lt 2 ]
-then
- echo "usage: $0 <jail-folder> <binary> [<binary> ...]" >&2
- exit 1
-fi
+CHROOT_DIR=/home/admin/jail
 
-TARGETDIR="$1"
-if [ ! -d "$TARGETDIR" ]
-then
- echo "target folder $TARGETDIR does not exist" >&2
- exit 1
-fi
+REQUIRED_CHROOT_FILES=" "
 
-shift 1
+cd $CHROOT_DIR
 
-if [ -z "$KEEP_LINKS" ]
-then
- CPIO_ARGS="-L"
-else
- CPIO_ARGS=""
-fi
+# Copy $REQUIRED_CHROOT_FILES and shared library dependencies
+# to chroot environment
 
-transfer()
-{
- echo "$1" | cpio -p --preserve-modification-time --make-directories $CPIO_ARGS "$TARGETDIR" &>/dev/null
- if [ $? -ne 0 ]
- then
-  echo "failed" >&2
-  exit 2
- else
-  echo "ok" >&2
- fi
-}
-
-qualifyLink()
-{
- DEREF=`readlink "$1"`
- DIRNAME=`dirname "$1"`
-
- echo "$DIRNAME/$DEREF"
-}
-
-transferLL()
-{
- for LIBRARY in `ldd "$1" | grep -e '(0x' | awk '{if($2=="=>")print $3;else print $1}'`
- do
-  echo -n "$2 - depends on $LIBRARY ... " >&2
-  transfer "$LIBRARY"
-
-  if [ -n "$KEEP_LINKS" ]
-  then
-   LINKED="$LIBRARY"
-   while [ -L "$LINKED" ]
-   do
-    LINKED=`qualifyLink "$LINKED"`
-    echo -n "$2    - linked with $LINKED ... " >&2
-    transfer "$LINKED"
-   done
-  fi
-
-  if [ -L "$LIBRARY" ]
-  then
-   LIBRARY=`qualifyLink "$LIBRARY"`
-  fi
-
-  transferLL "$LIBRARY" "$2   "
- done
-}
-
-HAS_BINARY=0
-
-for NAME in "$@"
+for FILE in $REQUIRED_CHROOT_FILES
 do
- if [ "${NAME#*/}" = "$NAME" -a "${NAME%/*}" = "$NAME" ]
- then
-  TYPE="binary"
-  REALPATH=`which "$NAME" 2>/dev/null`
- elif [ ! -e "$NAME" ]
- then
-  echo "ignoring $NAME - not found" >&2
-  continue
- else
-  REALPATH="$NAME"
-
-  file "$NAME" 2>/dev/null | grep " ELF " &>/dev/null
-  if [ $? -eq 0 ]
-  then
-   TYPE="binary"
-  elif [ -d "$NAME" ]
-  then
-   TYPE="folder"
-  elif [ -b "$NAME" -o -c "$NAME" ]
-  then
-   TYPE="device"
-  elif [ -S "$NAME" ]
-  then
-   TYPE="socket"
-  else
-   TYPE="file"
-  fi
- fi
-
- echo -n "processing $TYPE $REALPATH ... " >&2
- transfer "$REALPATH"
-
- if [ "$TYPE" = "binary" ]
- then
-  HAS_BINARY=1
-  transferLL "$REALPATH"
- fi
+   DIR=`dirname $FILE | cut -c2-`
+   [ ! -d $DIR ] && mkdir -p $DIR
+   cp $FILE `echo $FILE | cut -c2-`
+   for SHARED_LIBRARY in `ldd $FILE | awk '{print $NF}'`
+   do
+      DIR=`dirname $SHARED_LIBRARY | cut -c2-`
+      [ ! -d $DIR ] && mkdir -p $DIR
+      [ ! -s "`echo $SHARED_LIBRARY | cut -c2-`" ] && \
+      cp $SHARED_LIBRARY `echo $SHARED_LIBRARY | cut -c2-`
+   done
 done
 
-if [ $HAS_BINARY -ne 0 ]
-then
- echo -n "integrating ld cache ... "
- transfer /etc/ld.so.cache
- echo -n "integrating ld configuration ... "
- transfer /etc/ld.so.conf
-fi
+cp /usr/lib/ld.so.1 usr/lib
